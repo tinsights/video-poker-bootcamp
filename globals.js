497c938ffe1e21ed30c7bbd809fe057d28765e2c
Tinaes <aj.tinaes@gmail.com>
2021-07-28 15:35:15 +0800

    removed vestigial functions

--- globals.js
+++ globals.js
@@ -1,7 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable prefer-const */
-// GLOBAL DOM ELEMENTS
-// const mainContainer = document.createElement('div');
 const mainContainer = document.getElementById('main-container');
 const startButton = document.getElementById('dealBtn');
 const replaceButton = document.getElementById('replaceBtn');
@@ -15,23 +11,13 @@ const crtEffect = document.createElement('div');
 
 crtEffect.className = 'crt';
 cardContainer.className = 'card-container';
-// mainContainer.id = 'main-container';
-// startButton.innerText = 'Start New Game';
-// replaceButton.innerText = 'Replace Cards';
-// submitButton.innerText = 'Done!';
-
 mainContainer.appendChild(cardContainer);
 mainContainer.appendChild(crtEffect);
-// document.body.appendChild(startButton);
-// document.body.appendChild(replaceButton);
-// document.body.appendChild(submitButton);
-// document.body.appendChild(mainContainer);
+
 document.body.appendChild(gameInfo);
 
 document.addEventListener('DOMContentLoaded', () => {
   startButton.addEventListener('click', initGame);
-  // replaceButton.addEventListener('click', swapCards);
-  // submitButton.addEventListener('click', calcScore);
   addBetButton.addEventListener('click', betIncrement);
   subBetButton.addEventListener('click', betDecrement);
 });
@@ -39,10 +25,10 @@ document.addEventListener('DOMContentLoaded', () => {
 // GLOBAL Constants
 const suits = ['H', 'D', 'C', 'S'];
 const symbols = ['♥', '♦', '♣', '♠'];
-let deck = [];
-let hand = [];
-let displayHand = '';
+const deck = [];
+const hand = [];
+const displayHand = '';
 const odds = [1, 2, 3, 4, 6, 9, 25, 50, 250];
-let credit = 100;
-let bet = 1;
-let gameMode = 0;
+const credit = 100;
+const bet = 1;
+const gameMode = 0;
